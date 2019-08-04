c97cb9f0b3605ef5bd1479aa145a00e2432859fb
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-08-04 17:19:59 -0400

    tired, calling it a night

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+class ThankYouView(TemplateView):
+    template_name = 'pages/verify_email.html'
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
--- users/forms.py
+++ users/forms.py
@@ -12,24 +12,30 @@ class CustomUserCreationForm(UserCreationForm):
         layout = helper.layout = Layout()
         self.fields['password1'].required = False
         self.fields['password2'].required = False
-        self.fields['password1'].label = 'TEST'
-        self.fields['password1'].widget.attrs['placeholder'] = 'TEST'
         print(self.fields['password1'].widget.attrs)
         layout.append(
             Field('password1', placeholder='THIS IS YOUR PASSWORD')
         )
         #for field_name, field in self.fields.items():
         #    layout.append(Field(field_name, placeholder=field.label))
-
+        del self.fields['password1']
+        del self.fields['password2']
         helper.form_show_labels = False
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'email')
 
-class CustomUserChangeForm(UserChangeForm):
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = super(UserCreationForm, self).clean_password2()
+        if bool(password1) ^ bool(password2):
+            raise forms.ValidationError("Fill out both fields")
+        return password2
+
+
+class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
         fields = ('username', 'email')
-
