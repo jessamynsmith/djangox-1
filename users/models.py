f9fc974275d6224d061d0bda295ef2a1cb200ad2
Francisco Javier Arceo <arceofrancisco@gmail.com>
2019-08-05 19:35:03 -0400

    uploading latest changes

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
-    password1 = models.CharField(_('password'), max_length=128, blank=True)
 
     def __str__(self):
         return self.email
