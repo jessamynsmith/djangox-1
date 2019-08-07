f656928b737a77b5cfbaf0d4d4eaaa916b62477a
jessamynsmith <jessamyn.smith@gmail.com>
2019-08-07 15:13:01 -0600

    cleaned up unnecessary code

--- users/forms.py
+++ users/forms.py
@@ -1,32 +1,18 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(forms.ModelForm):
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = CustomUser
         fields = ('email',)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        print('__init__')
-        # self.fields['password1'].required = False
-        #self.fields['password1'].widget = forms.HiddenInput()
-
-    def clean(self):
-        result = super().clean()
-        print('cleaning')
-        return result
-
     def save(self, commit=True):
         instance = super().save(commit=False)
-        print('1', instance.email, instance.username)
         instance.set_unusable_password()
-        print('1', instance.email, instance.username)
         instance.username = instance.email.split('@')[0]
-        print('3', instance.email, instance.username)
         if commit:
             instance.save()
         return instance
--- users/views.py
+++ users/views.py
@@ -5,11 +5,3 @@ from users.forms import CustomUserCreationForm
 
 class MySignupView(SignupView):
     form_class = CustomUserCreationForm
-
-    # def get_form_class(self):
-    #     return get_form_class(app_settings.FORMS, 'signup', self.form_class)
-
-    def dispatch(self, request, *args, **kwargs):
-        print('MySignupView')
-        response = super().dispatch(request, *args, **kwargs)
-        return response
